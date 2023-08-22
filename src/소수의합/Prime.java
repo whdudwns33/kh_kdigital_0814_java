0bbc55a71219d0d7ef53a583df7722cd775f61c3
jojo6807 <jyj05181@gmail.com>
2023-08-22 17:56:56 +0900

    2와 3의 배수를 제외한 소수 더하기

--- src/소수의합/Prime.java
+++ src/소수의합/Prime.java
@@ -3,26 +3,26 @@ package 소수의합;
 import java.util.Scanner;
 
 public class Prime {
-    /*
+
     private int num;
     private int sum;
 
-    Prime() {
+    Prime(int i) {
+
     }
 
     public void setSum(int num) {
         this.num = num;
-        for (int i = 0; i < num; i++) {
+        for (int i = 2; i < num; i++) {
             if (num < 3) {
                 sum = 2;
             } else if (num < 4) {
                 sum = 2 + 3;
-            } else if (num >= 4 && i%2 != 0 && i%3 != 0) {
-                sum = 5 + i;
+                if (num >= 4 && i % 2 != 0 && i % 3 != 0) {
+                    sum += i;
+                }
             }
+            System.out.println(sum + 5);
         }
-        System.out.println(sum);
     }
-     */
-
 }
