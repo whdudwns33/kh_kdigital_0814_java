d70e432154f83493623f564810db389bd27e1cb7
jojo6807 <jyj051811@gmail.com>
2023-08-25 18:57:16 +0900

    깃 반영

--- src/깃불러오기/Git.java
+++ src/깃불러오기/Git.java
@@ -1,7 +1,10 @@
 package 깃불러오기;
-
+// git status
 
 // 깃 불러오기
+// git을 저장할 폴더 설정
+// cd c:
+// cd folder 명
 // //git clone 주소복사
 // cd 파일명
 // git pull
@@ -12,5 +15,11 @@ package 깃불러오기;
 // git commit
 // git remote ~~~
 // git push ~~~
+
+
+// 인텔리제이 내부에서 사용하는 git 터미널
+// git add .
+// git commit -m "내용: 0830일자 수업 반영입니다."
+// git push - u origin master
 public class Git {
 }
