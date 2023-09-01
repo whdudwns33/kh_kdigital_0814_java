2f6debceff09fed53b974ea7b3c02e3f613d6d6b
jojo6807 <jyj05181@gmail.com>
2023-09-01 09:05:17 +0900

    .

--- src/배열관리/NameCard.java
+++ src/배열관리/NameCard.java
@@ -1,4 +1,32 @@
 package 배열관리;
 
 public class NameCard {
+    private String name;
+    private String mail;
+    private String phone;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void printInfo () {
+        System.out.println("이름 :" + name);
+        System.out.println("나이 :" + age);
+        System.out.println("연락처 :" + phone);
+        System.out.println("메일 :" + mail);
+        System.out.println();
+    }
 }
